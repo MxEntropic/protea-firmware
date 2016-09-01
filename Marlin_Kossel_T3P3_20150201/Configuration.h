f5f1f8e5438ad4ae2fa8744e333bd8f8ca24ff7b
MsEntropic <ko14705@bristol.ac.uk>
2016-09-01 19:00:32 +0100

    step change and new nozzle
    adjusted new nozzle height and number of steps for extrusion

--- Marlin_Kossel_T3P3_20150201/Configuration.h
+++ Marlin_Kossel_T3P3_20150201/Configuration.h
@@ -355,7 +355,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // For deltabots this means top and center of the cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 267.2 // T3P3, with V6 and heated bed. With J-head and no heated bed start at 255
+#define MANUAL_Z_HOME_POS 267.1 // T3P3, with V6 and heated bed. With J-head and no heated bed start at 255
 
 #define AUTOLEVEL_GRID 22  // Distance between autolevel Z probing points, should be less than print surface radius/3.
 
@@ -367,7 +367,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 80, 1800} // T3P3 for 20T pulleys & RRP mini geared extruder
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 80, 350} // T3P3 for 20T pulleys & RRP mini geared extruder
 #define DEFAULT_MAX_FEEDRATE          {200, 200, 200, 200}    // (mm/sec) 
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,9000,9000}    // X, Y, Z, E maximum start speed for accelerated moves.
 
